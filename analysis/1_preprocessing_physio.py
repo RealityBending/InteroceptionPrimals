a909ac4f067df7129ae1cef888a5c7e4154bde7c
Dominique Makowski <dom.mak19@gmail.com>
2023-08-15 15:45:31 +0100

    script

--- analysis/1_preprocessing_physio.py
+++ analysis/1_preprocessing_physio.py
@@ -1,7 +1,7 @@
 import io
 import os
 
-# import autoreject
+import autoreject
 import matplotlib.pyplot as plt
 import mne
 import neurokit2 as nk
@@ -24,11 +24,11 @@ def fig2img(fig):
     return img
 
 
-def qc_physio(df, plot_ecg=[], plot_ppg=[]):
+def qc_physio(df, sub, plot_ecg=[], plot_ppg=[]):
     """Quality control (QC) of physiological signals."""
 
     # ECG ------------------------------------------------------------------------------------
-    nk.ecg_plot(df, sampling_rate=2000)  # Save ECG plot
+    nk.ecg_plot(df, sub, sampling_rate=2000)  # Save ECG plot
     fig = plt.gcf()
     img = ill.image_text(
         sub, color="black", size=100, x=-0.82, y=0.90, image=fig2img(fig)
@@ -42,46 +42,170 @@ def qc_physio(df, plot_ecg=[], plot_ppg=[]):
         sub, color="black", size=100, x=-0.82, y=0.90, image=fig2img(fig)
     )
     plot_ppg.append(img)
-    plt.close(fig)  # Do not show the plot in the console
+    plt.close(fig)
     return plot_ecg, plot_ppg
 
 
-# def qc_eeg(raw, plot_psd=[]):
-#     """Quality control (QC) of PSD."""
-#     psd = raw.compute_psd(picks="eeg", fmax=80).to_data_frame()
-#     psd.plot(x="freq", y=["AF7", "AF8", "TP9", "TP10"])
-#     plt.yscale("log")
-#     fig = plt.gcf()
-#     img = ill.image_text(
-#         sub, color="black", size=100, x=-0.82, y=0.90, image=fig2img(fig)
-#     )
-#     plt.close(fig)  # Do not show the plot in the console
-#     plot_psd.append(img)
-#     return plot_psd
+def qc_eeg(raw, sub, plot_psd=[]):
+    """Quality control (QC) of EEG."""
+    ch_names = ["AF7", "AF8", "TP9", "TP10"]
+
+    fig1, ax = plt.subplots(4, 1, figsize=(6, 5))
+    fig1.suptitle(f"{sub}")
+    raw.to_data_frame()[ch_names].plot(ax=ax, subplots=True, linewidth=0.5)
+
+    # PSD
+    fig2, ax = plt.subplots(1, 1, figsize=(6, 5))
+    psd = raw.compute_psd(picks="eeg", n_fft=256 * 20, fmax=80).to_data_frame()
+    psd.plot(x="freq", y=ch_names, ax=ax, logy=True)
+
+    img = ill.image_mosaic([fig2img(i) for i in [fig1, fig2]], nrows=1, ncols=2)
+
+    # To image
+    plot_psd.append(img)
+    plt.close("all")
+    return plot_psd
+
+
+def qc_hep(epochs, reject_log, sub, plot_hep=[]):
+    """Quality control (QC) of HEPs."""
+    fig, ax = plt.subplots(1, 2, figsize=(12, 5))
+    fig.suptitle(f"{sub}")
+
+    if reject_log is not None:
+        reject_log.plot("horizontal", aspect="auto", ax=ax[0], show=False)
+
+    hep = epochs.average(picks=["AF7", "AF8"], method=lambda x: np.nanmean(x, axis=0))
+    hep.to_data_frame().plot(x="time", y=["AF7", "AF8"], ax=ax[1])
+
+    # To image
+    plot_hep.append(fig2img(fig))
+    plt.close("all")
+    return plot_hep
+
+
+def qc_heo(power, itc, sub, plot_heo=[]):
+    fig1 = power.plot_topo(
+        baseline=None,
+        mode="logratio",
+        title=f"{sub}:  Time-frequency Power",
+        vmin=0,
+        # vmax=1e-7,
+        fig_facecolor="white",
+        fig_background=None,
+        font_color="black",
+        show=False,
+    )
+
+    fig2 = itc.plot_topo(
+        title="Inter-Trial coherence",
+        vmin=0,
+        vmax=1.0,
+        cmap="CMRmap",
+        fig_facecolor="white",
+        fig_background=None,
+        font_color="black",
+        show=False,
+    )
+
+    img = ill.image_mosaic([fig2img(i) for i in [fig1, fig2]], nrows=1, ncols=2)
+    plot_heo.append(img)
+    plt.close("all")
+    return plot_heo
+
+
+def analyze_hep(raw, ecg=None, ppg=None):
+    out = {}
+    # Preprocess physio
+    out["ecg"], _ = nk.bio_process(ecg=ecg, ppg=ppg, sampling_rate=raw.info["sfreq"])
+
+    # Find R-peaks
+    events, _ = nk.events_to_mne(out["ecg"]["ECG_R_Peaks"].values.nonzero()[0])
+    epochs = mne.Epochs(
+        raw,
+        events,
+        tmin=-0.3,
+        tmax=0.7,
+        detrend=0,
+        decim=4,  # Downsample to 500 Hz
+        verbose=False,
+        preload=True,
+    )
+
+    # Autoreject
+    try:
+        ar = autoreject.AutoReject(verbose=False, picks="eeg")
+        epochs, out["autoreject_log"] = ar.fit_transform(epochs, return_log=True)
+    except IndexError:
+        out["autoreject_log"] = None
+    out["epochs"] = epochs
+
+    # Compute HEP Features
+    hep = epochs.average(
+        picks=["AF7", "AF8", "ECG", "PPG_Muse"],
+        method=lambda x: np.nanmean(x, axis=0),
+    ).to_data_frame()
+    hep.index = hep["time"]
+
+    hep1 = hep[0.2:0.4][["AF7", "AF8"]]
+    hep2 = hep[0.4:0.6][["AF7", "AF8"]]
+    rez = pd.DataFrame(
+        {
+            "HEP_Amplitude_200_400_EEG": [hep1.mean(axis=1).mean()],
+            "HEP_Amplitude_400_600_EEG": [hep2.mean(axis=1).mean()],
+        }
+    )
+
+    for ch in hep1.columns:
+        rez[f"HEP_Amplitude_200_400_{ch}"] = hep1[ch].mean()
+        rez[f"HEP_Amplitude_400_600_{ch}"] = hep2[ch].mean()
+
+    # Compute Time-frequency Power
+    freqs = np.logspace(*np.log10([6, 30]), num=8)  # freqs of interest (log-spaced)
+    n_cycles = freqs / 2.0  # different number of cycle per frequency
+    power, itc = mne.time_frequency.tfr_morlet(
+        epochs,
+        freqs=freqs,
+        n_cycles=n_cycles,
+        use_fft=True,
+        return_itc=True,
+    )
+    out["timefrequency"] = power
+    out["itc"] = itc
+
+    # Compute HEO Features
+    # power.to_data_frame()
+
+    return rez, out
 
 
 # Variables ==================================================================================
 # Change the path to your local data folder.
 # The data can be downloaded from OpenNeuro (TODO).
-path = "C:/Users/domma/Box/Data/PrimalsInteroception/Reality Bending Lab - PrimalsInteroception/"
+# path = "C:/Users/domma/Box/Data/PrimalsInteroception/Reality Bending Lab - PrimalsInteroception/"
+path = "C:/Users/dmm56/Box/Data/PrimalsInteroception/Reality Bending Lab - PrimalsInteroception/"
 
 # Get participant list
 meta = pd.read_csv(path + "participants.tsv", sep="\t")
 
-
 # Initialize variables
+df = pd.DataFrame()
+df_hep = pd.DataFrame()
+
 qc_hct_ecg = []
 qc_hct_ppg = []
 qc_rs_psd = []
+qc_rs_ecg = []
+qc_rs_ppg = []
 qc_rs_hep = []
-df = pd.DataFrame()
+qc_rs_heo = []
 
-# sub = "sub-02"
+# sub = "sub-07"
 # Loop through participants ==================================================================
 for sub in meta["participant_id"].values:
     # Print progress and comments
     print(sub)
-    print("  - " + meta[meta["participant_id"] == sub]["Comments"].values[0])
+    print("  * " + meta[meta["participant_id"] == sub]["Comments"].values[0])
 
     # Path to EEG data
     path_eeg = path + sub + "/eeg/"
@@ -92,126 +216,105 @@ for sub in meta["participant_id"].values:
     file = path_beh + [file for file in file if ".tsv" in file][0]
     dfsub = pd.read_csv(file, sep="\t")
 
-    # # Heartbeat Evoked Potentials (HEP) ------------------------------------------------------
-    # # Open RS file
-    # file = [file for file in os.listdir(path_eeg) if "RS" in file]
-    # file = path_eeg + [file for file in file if ".vhdr" in file][0]
-    # rs = mne.io.read_raw_brainvision(file, preload=True)
-    # rs = rs.set_montage("standard_1020")
+    # Resting State ==========================================================================
 
-    # # Preprocess
-    # rs, _ = mne.set_eeg_reference(rs, ["TP9", "TP10"])
-    # rs = rs.filter(1, 40, fir_design="firwin")
+    # Preprocessing --------------------------------------------------------------------------
+    print("  - RS - Preprocessing")
 
-    # # NK pipeline
-    # df = rs.to_data_frame()[["time", "AF7", "AF8", "PHOTO", "ECG"]]
-    # df["EEG"] = df[["AF7", "AF8"]].mean(axis=1) / 1000000
-    # events = nk.events_find(df["PHOTO"], threshold_keep="below", duration_min=50000)
-    # df = df.iloc[events["onset"][0] : events["onset"][0] + events["duration"][0]]
-    # rs_physio, _ = nk.ecg_process(df["ECG"].values, sampling_rate=rs.info["sfreq"])
-    # epochs = nk.epochs_create(
-    #     df,
-    #     rs_physio["ECG_R_Peaks"].values.nonzero()[0],
-    #     epochs_start=-0.5,
-    #     epochs_end=1,
-    # )
-    # for i, epoch in epochs.items():
-    #     plt.plot(epoch.index, epoch["EEG"], color="red", alpha=0.1)
-    #     # plt.plot(epoch.index, epoch["AF8"], color="blue", alpha=0.1)
-    #     plt.ylim(-2, 2)
+    # Open RS assessment
+    file = [file for file in os.listdir(path_beh) if "RS" in file]
+    file = path_beh + [file for file in file if ".tsv" in file][0]
+    rs_beh = pd.read_csv(file, sep="\t").drop(["participant_id"], axis=1)
+    dfsub = pd.concat([dfsub, rs_beh.add_prefix("RS_")], axis=1)
 
-    # # Crops to RS time
-    # events = nk.events_find(
-    #     rs.to_data_frame()["PHOTO"], threshold_keep="below", duration_min=50000
-    # )
-    # assert len(events["onset"]) == 1  # Check that there is only one event
-    # rs = nk.mne_crop(
-    #     rs, smin=events["onset"][0], smax=events["onset"][0] + events["duration"][0]
-    # )
-    # # rs = rs.crop(
-    # #     tmin=rs.times[events["onset"][0]],
-    # #     tmax=rs.times[events["onset"][0] + events["duration"][0]],
-    # # )
+    # Open RS file
+    file = [file for file in os.listdir(path_eeg) if "RS" in file]
+    file = path_eeg + [file for file in file if ".vhdr" in file][0]
+    rs = mne.io.read_raw_brainvision(file, preload=True)
 
-    # # QC
-    # qc_eeg(rs, plot_psd=signals_qc_rs_psd)
-    # # rs.plot_sensors(show_names=True)
-    # # rs.plot()
+    # Detect onset of RS
+    events = nk.events_find(
+        rs.to_data_frame()["PHOTO"],
+        threshold_keep="below",
+        duration_min=int(rs.info["sfreq"] * 5),
+    )
+    assert len(events["onset"]) == 1  # Check that there is only one event
 
-    # # Autoreject
-    # chunks = mne.make_fixed_length_events(rs, duration=1.0)
-    # epochs = mne.Epochs(rs, chunks, tmin=0, tmax=1.0, baseline=None, preload=True)
-    # if sub in ["sub-04", "sub-06"]:  # These ppts error
-    #     reject = {}
-    # else:
-    #     reject = autoreject.get_rejection_threshold(epochs, verbose=False)
-    #     thresholds = autoreject.compute_thresholds(epochs, method="random_search")
+    # Filter EEG
+    rs = rs.set_montage("standard_1020")
+    rs, _ = mne.set_eeg_reference(rs, ["TP9", "TP10"])
+    rs = rs.notch_filter(np.arange(50, 251, 50), picks="eeg")
+    rs = rs.filter(1, 40, picks="eeg")
+    rs = nk.mne_crop(
+        rs, smin=events["onset"][0], smax=events["onset"][0] + events["duration"][0]
+    )
 
-    # # Find R-peaks
-    # rs_physio, _ = nk.ecg_process(
-    #     rs.to_data_frame()["ECG"].values, sampling_rate=rs.info["sfreq"]
-    # )
-    # events, event_id = nk.events_to_mne(rs_physio["ECG_R_Peaks"].values.nonzero()[0])
-    # # events2, _, _ = mne.preprocessing.find_ecg_events(rs, event_id=0, ch_name="ECG")
-    # epochs = mne.Epochs(
-    #     rs,
-    #     events,
-    #     # event_id=event_id,
-    #     picks="eeg",
-    #     tmin=-0.3,
-    #     tmax=0.7,
-    #     verbose=False,
-    #     preload=True,
-    # )
-    # epochs = epochs.drop_bad(reject=reject)
-    # # epochs.plot_drop_log()
-    # # fig = epochs.plot(events=events)
+    # Fix for recording interruption (cut till before the nan)
+    if sub in ["sub-10"]:
+        first_na = np.where(rs.to_data_frame()[["AF7"]].isna())[0][0]
+        rs = nk.mne_crop(rs, smin=0, smax=first_na - 1)
 
-    # df_hep = epochs.to_data_frame()
-    # df_hep["EEG"] = df_hep[["AF7", "AF8"]].mean(axis=1)
-    # df_hep["participant_id"] = sub
-    # signals_qc_rs_hep.append(df_hep[["participant_id", "epoch", "time", "EEG"]])
+    # QC
+    qc_rs_psd = qc_eeg(rs, sub, plot_psd=qc_rs_psd)
 
-    # hep = epochs.average(picks=["AF7", "AF8"], method=lambda x: np.nanmean(x, axis=0))
-    # signals_qc_rs_hep.append(hep)
-    # # hep.to_data_frame()
+    # Heartbeat Evoked Potentials (HEP) -------------------------------------------------------
+    print("  - RS - HEP")
 
-    # # hep.plot(gfp=True)
-    # # grand_average = mne.grand_average([hep])
-    # # grand_average.plot()
+    rs_hep, out = analyze_hep(rs, ecg=rs["ECG"][0][0], ppg=rs["PPG_Muse"][0][0])
+
+    # QC
+    qc_rs_ecg, qc_rs_ppg = qc_physio(
+        out["ecg"], sub, plot_ecg=qc_rs_ecg, plot_ppg=qc_rs_ppg
+    )
+    qc_rs_hep = qc_hep(out["epochs"], out["autoreject_log"], sub, plot_hep=qc_rs_hep)
+    qc_rs_heo = qc_heo(out["timefrequency"], out["itc"], sub, plot_heo=qc_rs_heo)
+
+    # Add Features
+    if sub not in ["sub-06", "sub-09"]:
+        dfsub = pd.concat([dfsub, rs_hep], axis=1)
 
     # Heartbeat Counting Task (HCT) ----------------------------------------------------------
+    print("  - HCT - Preprocessing")
     # Open HCT file
     file = [file for file in os.listdir(path_eeg) if "HCT" in file]
     file = path_eeg + [file for file in file if ".vhdr" in file][0]
     hct = mne.io.read_raw_brainvision(file, preload=True, verbose=False)
-    hct = hct.to_data_frame()[["PHOTO", "ECG", "PPG_Muse"]]
-    # Remove NA
-    for m in ["bfill", "ffill"]:
-        hct = hct.fillna(method=m)
 
     # Find events and crop just before (1 second +/-) first and after last
-    events = nk.events_find(hct["PHOTO"], threshold_keep="below", duration_min=15000)
+    events = nk.events_find(
+        hct["PHOTO"][0][0], threshold_keep="below", duration_min=15000
+    )
     start_end = [events["onset"][0], events["onset"][-1] + events["duration"][-1]]
     if sub in ["sub-13"]:
-        start_end[0] = 2000
-    hct = hct.iloc[start_end[0] - 2000 : start_end[1] + 2000]
+        start_end[0] = 2178
+    hct = nk.mne_crop(hct, smin=start_end[0] - 2000, smax=start_end[1] + 2000)
+    # Find events (again as data was cropped) and epoch
+    events = nk.events_find(
+        hct["PHOTO"][0][0], threshold_keep="below", duration_min=15000
+    )
+    assert len(events["onset"]) == 6  # Check that there are 6 epochs (the 6 intervals)
+
+    # HCT - HEP ------------------------------------------------------------------------------
+    print("  - HCT - HEP")
+
+    # HCT - Task -----------------------------------------------------------------------------
+    print("  - HCT - Task")
 
     # Process signals
-    hct, _ = nk.bio_process(
-        ecg=hct["ECG"].values,
-        ppg=hct["PPG_Muse"],
+    hct_physio = hct.to_data_frame()[["PHOTO", "ECG", "PPG_Muse"]]
+    hct_physio, _ = nk.bio_process(
+        ecg=hct_physio["ECG"].values,
+        ppg=hct_physio["PPG_Muse"].values,
         sampling_rate=2000,
-        keep=hct["PHOTO"],
+        keep=hct_physio["PHOTO"],
     )
-    qc_hct_ecg, qc_hct_ppg = qc_physio(hct, plot_ecg=qc_hct_ecg, plot_ppg=qc_hct_ppg)
+    qc_hct_ecg, qc_hct_ppg = qc_physio(
+        hct_physio, sub, plot_ecg=qc_hct_ecg, plot_ppg=qc_hct_ppg
+    )
 
-    # Find events (again as data was cropped) and epoch
-    events = nk.events_find(hct["PHOTO"], threshold_keep="below", duration_min=15000)
     epochs = nk.epochs_create(
-        hct, events, sampling_rate=2000, epochs_start=0, epochs_end="from_events"
+        hct_physio, events, sampling_rate=2000, epochs_start=0, epochs_end="from_events"
     )
-    assert len(epochs) == 6  # Check that there are 6 epochs (the 6 intervals)
 
     # Load behavioral data
     file = [file for file in os.listdir(path_beh) if "HCT" in file]
@@ -245,40 +348,28 @@ for sub in meta["participant_id"].values:
 
     df = pd.concat([df, dfsub], axis=0)
 
+
 # Clean up and Save data
 df = pd.merge(meta, df)
 # Keep only columns that do not end with number or _R
-df = df.filter(regex="^(?!.*[0-9]$)(?!.*_R$).*")
+# df = df.filter(regex="^(?!.*[0-9]$)(?!.*_R$).*")
 df.to_csv("../data/data.csv", index=False)
+df_hep.to_csv("../data/data_hep.csv", index=False)
 
 # Save figures
+qc_rs_psd = ill.image_mosaic(qc_rs_psd, ncols=2, nrows="auto")
+qc_rs_psd.save("figures/signals_qc_rs_psd.png")
+qc_rs_ecg = ill.image_mosaic(qc_rs_ecg, ncols=2, nrows="auto")
+qc_rs_ecg.save("figures/signals_qc_rs_ecg.png")
+qc_rs_ppg = ill.image_mosaic(qc_rs_ppg, ncols=2, nrows="auto")
+qc_rs_ppg.save("figures/signals_qc_rs_ppg.png")
+qc_rs_hep = ill.image_mosaic(qc_rs_hep, ncols=2, nrows="auto")
+qc_rs_hep.save("figures/signals_qc_rs_hep.png")
+qc_rs_heo = ill.image_mosaic(qc_rs_heo, ncols=2, nrows="auto")
+qc_rs_heo.save("figures/signals_qc_rs_heo.png")
 qc_hct_ecg = ill.image_mosaic(qc_hct_ecg, ncols=2, nrows="auto")
 qc_hct_ecg.save("figures/signals_qc_hct_ecg.png")
 qc_hct_ppg = ill.image_mosaic(qc_hct_ppg, ncols=2, nrows="auto")
 qc_hct_ppg.save("figures/signals_qc_hct_ppg.png")
-# signals_qc_rs_psd = ill.image_mosaic(signals_qc_rs_psd, ncols=2, nrows="auto")
-# signals_qc_rs_psd.save("figures/signals_qc_rs_psd.png")
-
-# fig = plt.figure()
-# for sub in signals_qc_rs_psd["participant_id"].unique():
-#     dat = signals_qc_rs_psd[signals_qc_rs_psd["participant_id"] == sub]
-#     fig = dat.plot(x="freq", y=["AF7", "AF8", "TP9", "TP10"])
-# fig2img(fig)
-
-
-# %matplotlib inline
-# signals_qc_rs_hep = pd.concat(signals_qc_rs_hep, axis=0)
-# for sub in signals_qc_rs_hep["participant_id"].unique():
-#     dat = signals_qc_rs_hep[signals_qc_rs_hep["participant_id"] == sub]
-#     for epoch in dat["epoch"].unique():
-#         dat2 = dat[dat["epoch"] == epoch]
-#         plt.plot(dat2["time"], dat2["EEG"], color="black", alpha=0.02)
-# plt.show()
-# plt.close()
-# signals_qc_rs_hep[5].to_data_frame()
-# signals_qc_rs_hep.pop(4)
-# ga = mne.grand_average(signals_qc_rs_hep)
-# mne.channels.combine_channels(ga, {"HEP":[0, 1]}, method="mean").plot()
-# ga.plot()
 
 print("Done!")
