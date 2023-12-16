4b085813c4594a2970892006772d22bc7e7b6a91
Dominique Makowski <dom.mak19@gmail.com>
2023-12-16 14:42:43 +0000

    preprocesssing script

--- analysis/1_preprocessing_physio.py
+++ analysis/1_preprocessing_physio.py
@@ -38,14 +38,15 @@ def qc_physio(df, info, sub, plot_ecg=[], plot_ppg=[]):
     plot_ecg.append(img)
 
     # PPG ------------------------------------------------------------------------------------
-    df["PPG_Raw"] = nk.rescale(df["PPG_Raw"], to=df["PPG_Clean"])
-    nk.ppg_plot(df, info)  # Save ECG plot
-    fig = plt.gcf()
-    img = ill.image_text(
-        sub, color="black", size=100, x=-0.82, y=0.90, image=fig2img(fig)
-    )
-    plot_ppg.append(img)
-    plt.close(fig)
+    if plot_ppg is not None:
+        df["PPG_Raw"] = nk.rescale(df["PPG_Raw"], to=df["PPG_Clean"])
+        nk.ppg_plot(df, info)  # Save ECG plot
+        fig = plt.gcf()
+        img = ill.image_text(
+            sub, color="black", size=100, x=-0.82, y=0.90, image=fig2img(fig)
+        )
+        plot_ppg.append(img)
+        plt.close(fig)
     return plot_ecg, plot_ppg
 
 
@@ -191,6 +192,140 @@ def analyze_hep(raw, events):
     return rez, out
 
 
+# Processing =================================================================================
+
+
+def process_tap(sub, path_eeg, path_beh, qc_tap_ecg=[]):
+    # Open TAP file
+    file = [file for file in os.listdir(path_eeg) if "TAP" in file]
+    file = path_eeg + [f for f in file if ".vhdr" in f][0]
+    tap = mne.io.read_raw_brainvision(file, preload=True, verbose=False)
+
+    # Load behavioral data
+    file = [file for file in os.listdir(path_beh) if "TAP" in file]
+    file = path_beh + [f for f in file if ".tsv" in f][0]
+    tap_beh = pd.read_csv(file, sep="\t")
+
+    # Find events and crop just before (1 second +/-) first and after last
+    events = nk.events_find(
+        tap["PHOTO"][0][0],
+        threshold_keep="below",
+        duration_min=50,
+        duration_max=500,
+    )
+    # In milliseconds (= /2 for 2000 Hz)
+    onsets_photo = events["onset"] / (tap.info["sfreq"] / 1000)
+
+    # If long first event, remove it
+    if (len(onsets_photo) == 421) & (
+        np.diff(onsets_photo)[0] > np.mean(np.diff(onsets_photo)[1:61]) * 3
+    ):
+        onsets_photo = onsets_photo[1::]
+
+    # Manual fix
+    # plt.vlines(onsets_photo, 0, 1, color="blue")
+    # plt.vlines(tap_beh["Tapping_Times"].values + onsets_photo[0], 1, 2, color="red")
+    if sub in ["sub-01", "sub-17", "sub-22", "sub-26"]:
+        onsets_photo = onsets_photo[1::]
+
+    if len(onsets_photo) != 420:
+        print(f"    - WARNING: Number of events is not 420 ({len(onsets_photo)})")
+
+    # Correct for delay between photo and behavioral data
+    onsets_beh = tap_beh["Tapping_Times"].values + onsets_photo[0]
+
+    # Compute correlation
+    if scipy.stats.pearsonr(onsets_photo, onsets_beh)[0] < 0.999:
+        # plt.scatter(onsets_beh, onsets_photo)
+        print(f"    - WARNING: Correlation between photo and beh onsets is low.")
+
+    # Preprocess physio
+    bio = tap.to_data_frame().bfill()  # Backfill missing values at the beginning
+    bio, info = nk.bio_process(
+        ecg=bio["ECG"].values,
+        rsp=bio["RSP"].values,
+        sampling_rate=tap.info["sfreq"],
+    )
+
+    # QC
+    qc_tap_ecg, _ = qc_physio(bio, info, sub, plot_ecg=qc_tap_ecg, plot_ppg=None)
+
+    # Epoch around each tap
+    epochs = nk.epochs_create(
+        bio,
+        onsets_beh,  # onsets_photo
+        sampling_rate=tap.info["sfreq"],
+        epochs_start=-4,
+        epochs_end=4,
+    )
+
+    # Closest R-peak to each tap
+    dat = pd.DataFrame()  # Initialize dataframe
+    for _, e in epochs.items():
+        # Filter df at 0
+        at_index = e.iloc[np.argmin(np.abs(e.index)), :]
+        # R-peaks
+        rpeaks = e[e["ECG_R_Peaks"] == 1].index.values
+        r = np.nan if len(rpeaks) == 0 else rpeaks[np.argmin(np.abs(rpeaks))]
+        r_pre = np.nan if sum(rpeaks < 0) == 0 else np.max(rpeaks[rpeaks < 0])
+        r_post = np.nan if sum(rpeaks >= 0) == 0 else np.min(rpeaks[rpeaks >= 0])
+        # RSP - peaks
+        p = e[e["RSP_Peaks"] == 1].index.values
+        rsp_peak = np.nan if len(p) == 0 else p[np.argmin(np.abs(p))]
+        rsp_pre = np.nan if sum(p < 0) == 0 else np.max(p[p < 0])
+        rsp_post = np.nan if sum(p >= 0) == 0 else np.min(p[p >= 0])
+        # RSP - troughs
+        t = e[e["RSP_Troughs"] == 1].index.values
+        rsp_trough = np.nan if len(t) == 0 else t[np.argmin(np.abs(t))]
+        rsp_trough_pre = np.nan if sum(t < 0) == 0 else np.max(t[t < 0])
+        rsp_trough_post = np.nan if sum(t >= 0) == 0 else np.min(t[t >= 0])
+
+        dat = pd.concat(
+            [
+                dat,
+                pd.DataFrame(
+                    {
+                        "ECG_Rate": at_index["ECG_Rate"],
+                        "Closest_R": r,
+                        "Closest_R_Pre": r_pre,
+                        "Closest_R_Post": r_post,
+                        "ECG_Phase_Atrial": at_index["ECG_Phase_Atrial"],
+                        "ECG_Phase_Ventricular": at_index["ECG_Phase_Ventricular"],
+                        "Closest_RSP_Peak": rsp_peak,
+                        "Closest_RSP_Peak_Pre": rsp_pre,
+                        "Closest_RSP_Peak_Post": rsp_post,
+                        "Closest_RSP_Trough": rsp_trough,
+                        "Closest_RSP_Trough_Pre": rsp_trough_pre,
+                        "Closest_RSP_Trough_Post": rsp_trough_post,
+                        "RSP_Phase": at_index["RSP_Phase"],
+                        "RSP_Phase_Completion": at_index["RSP_Phase_Completion"],
+                    },
+                    index=[0],
+                ),
+            ],
+            axis=0,
+        )
+
+    # TODO: HEP amplitude of closest (pre) R-peak
+
+    # Compute tapping rate
+    tap_beh["Tapping_Rate"] = np.nan
+    for cond in tap_beh["Condition"].unique():
+        tap_times = tap_beh[tap_beh["Condition"] == cond]["Tapping_Times"].values
+        np.diff(tap_times)
+        rate = nk.signal_rate(
+            tap_times,
+            sampling_rate=1000,
+            show=False,
+            interpolation_method="monotone_cubic",
+        )
+        tap_beh.loc[tap_beh["Condition"] == cond, "Tapping_Rate"] = rate
+
+    # Merge with behavioral data
+    dat = pd.concat([tap_beh, dat.reset_index(drop=True)], axis=1)
+    return dat, qc_tap_ecg
+
+
 # Variables ==================================================================================
 # Change the path to your local data folder.
 # The data can be downloaded from OpenNeuro (TODO).
@@ -210,6 +345,7 @@ qc_rs_ecg = []
 qc_rs_ppg = []
 qc_rs_hep = []
 qc_rs_heo = []
+qc_tap_ecg = []
 qc_hct_psd = []
 qc_hct_ecg = []
 qc_hct_ppg = []
@@ -308,136 +444,15 @@ for sub in meta["participant_id"].values[0::]:
 
     # Tapping Task ===========================================================================
     print("  - TAP - Preprocessing")
+
     if sub in ["sub-09"]:
         print("    - WARNING: Skipping TAP for this participant (No ECG).")
-        continue
+    elif sub in ["sub-31"]:
+        print("    - WARNING: Skipping TAP for this participant (ECG electrode fell).")
+    else:
+        tap, qc_tap_ecg = process_tap(sub, path_eeg, path_beh, qc_tap_ecg)
+        df_tap = pd.concat([df_tap, tap], axis=0)
 
-    # Open TAP file
-    file = [file for file in os.listdir(path_eeg) if "TAP" in file]
-    file = path_eeg + [f for f in file if ".vhdr" in f][0]
-    tap = mne.io.read_raw_brainvision(file, preload=True, verbose=False)
-
-    # Load behavioral data
-    file = [file for file in os.listdir(path_beh) if "TAP" in file]
-    file = path_beh + [f for f in file if ".tsv" in f][0]
-    tap_beh = pd.read_csv(file, sep="\t")
-
-    # Find events and crop just before (1 second +/-) first and after last
-    events = nk.events_find(
-        tap["PHOTO"][0][0],
-        threshold_keep="below",
-        duration_min=50,
-        duration_max=500,
-    )
-    # In milliseconds (= /2 for 2000 Hz)
-    onsets_photo = events["onset"] / (tap.info["sfreq"] / 1000)
-
-    # If long first event, remove it
-    if (len(onsets_photo) == 421) & (
-        np.diff(onsets_photo)[0] > np.mean(np.diff(onsets_photo)[1:61]) * 3
-    ):
-        onsets_photo = onsets_photo[1::]
-
-    # Manual fix
-    # plt.vlines(onsets_photo, 0, 1, color="blue")
-    # plt.vlines(tap_beh["Tapping_Times"].values + onsets_photo[0], 1, 2, color="red")
-    if sub in ["sub-01", "sub-17", "sub-22"]:
-        onsets_photo = onsets_photo[1::]
-
-    if len(onsets_photo) != 420:
-        print(f"    - WARNING: Number of events is not 420 ({len(onsets_photo)})")
-
-    # Correct for delay between photo and behavioral data
-    onsets_beh = tap_beh["Tapping_Times"].values + onsets_photo[0]
-
-    # Compute correlation
-    if scipy.stats.pearsonr(onsets_photo, onsets_beh)[0] < 0.999:
-        # plt.scatter(onsets_beh, onsets_photo)
-        print(f"    - WARNING: Correlation between photo and beh onsets is low.")
-
-    # Preprocess physio
-    bio = tap.to_data_frame().bfill()  # Backfill missing values at the beginning
-    bio, info = nk.bio_process(
-        ecg=bio["ECG"].values,
-        ppg=bio["PPG_Muse"].values,
-        rsp=bio["RSP"].values,
-        sampling_rate=tap.info["sfreq"],
-    )
-
-    # Epoch around each tap
-    epochs = nk.epochs_create(
-        bio,
-        onsets_beh,  # onsets_photo
-        sampling_rate=tap.info["sfreq"],
-        epochs_start=-4,
-        epochs_end=4,
-    )
-
-    # Closest R-peak to each tap
-    dat = pd.DataFrame()  # Initialize dataframe
-    for _, e in epochs.items():
-        # Filter df at 0
-        at_index = e.iloc[np.argmin(np.abs(e.index)), :]
-        # R-peaks
-        rpeaks = e[e["ECG_R_Peaks"] == 1].index.values
-        r = np.nan if len(rpeaks) == 0 else rpeaks[np.argmin(np.abs(rpeaks))]
-        r_pre = np.nan if sum(rpeaks < 0) == 0 else np.max(rpeaks[rpeaks < 0])
-        r_post = np.nan if sum(rpeaks >= 0) == 0 else np.min(rpeaks[rpeaks >= 0])
-        # RSP - peaks
-        p = e[e["RSP_Peaks"] == 1].index.values
-        rsp_peak = np.nan if len(p) == 0 else p[np.argmin(np.abs(p))]
-        rsp_pre = np.nan if sum(p < 0) == 0 else np.max(p[p < 0])
-        rsp_post = np.nan if sum(p >= 0) == 0 else np.min(p[p >= 0])
-        # RSP - troughs
-        t = e[e["RSP_Troughs"] == 1].index.values
-        rsp_trough = np.nan if len(t) == 0 else t[np.argmin(np.abs(t))]
-        rsp_trough_pre = np.nan if sum(t < 0) == 0 else np.max(t[t < 0])
-        rsp_trough_post = np.nan if sum(t >= 0) == 0 else np.min(t[t >= 0])
-
-        dat = pd.concat(
-            [
-                dat,
-                pd.DataFrame(
-                    {
-                        "ECG_Rate": at_index["ECG_Rate"],
-                        "Closest_R": r,
-                        "Closest_R_Pre": r_pre,
-                        "Closest_R_Post": r_post,
-                        "ECG_Phase_Atrial": at_index["ECG_Phase_Atrial"],
-                        "ECG_Phase_Ventricular": at_index["ECG_Phase_Ventricular"],
-                        "Closest_RSP_Peak": rsp_peak,
-                        "Closest_RSP_Peak_Pre": rsp_pre,
-                        "Closest_RSP_Peak_Post": rsp_post,
-                        "Closest_RSP_Trough": rsp_trough,
-                        "Closest_RSP_Trough_Pre": rsp_trough_pre,
-                        "Closest_RSP_Trough_Post": rsp_trough_post,
-                        "RSP_Phase": at_index["RSP_Phase"],
-                        "RSP_Phase_Completion": at_index["RSP_Phase_Completion"],
-                    },
-                    index=[0],
-                ),
-            ],
-            axis=0,
-        )
-
-    # TODO: HEP amplitude of closest (pre) R-peak
-
-    # Compute tapping rate
-    tap_beh["Tapping_Rate"] = np.nan
-    for cond in tap_beh["Condition"].unique():
-        tap_times = tap_beh[tap_beh["Condition"] == cond]["Tapping_Times"].values
-        rate = nk.signal_rate(
-            tap_times,
-            sampling_rate=1000,
-            show=False,
-            interpolation_method="monotone_cubic",
-        )
-        tap_beh.loc[tap_beh["Condition"] == cond, "Tapping_Rate"] = rate
-
-
-    # Merge with behavioral data
-    dat = pd.concat([tap_beh, dat.reset_index(drop=True)], axis=1)
-    df_tap = pd.concat([df_tap, dat], axis=0)
 
 #     # Heartbeat Counting Task (HCT) ===========================================================
 
@@ -577,6 +592,8 @@ qc_rs_hep = ill.image_mosaic(qc_rs_hep, ncols=2, nrows="auto")
 qc_rs_hep.save("figures/signals_qc_rs_hep.png")
 qc_rs_heo = ill.image_mosaic(qc_rs_heo, ncols=2, nrows="auto")
 qc_rs_heo.save("figures/signals_qc_rs_heo.png")
+qc_tap_ecg = ill.image_mosaic(qc_tap_ecg, ncols=2, nrows="auto")
+qc_tap_ecg.save("figures/signals_qc_tap_ecg.png")
 qc_hct_ecg = ill.image_mosaic(qc_hct_ecg, ncols=2, nrows="auto")
 qc_hct_ecg.save("figures/signals_qc_hct_ecg.png")
 qc_hct_ppg = ill.image_mosaic(qc_hct_ppg, ncols=2, nrows="auto")
